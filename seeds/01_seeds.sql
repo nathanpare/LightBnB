190b5a82d490a7e372a55d8ad99d8fd893ab8e1c
Nathan Pare <nathanpare@Nathans-MacBook-Pro.local>
2022-04-30 13:52:15 -0700

    Fix naming conflict in 01_seeds

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -3,7 +3,7 @@ VALUES (1, 'Post Malone', 'postyrocks@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvme
 (2, 'Bobby Lee', 'thesleptking@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
 (3, 'Luan Oliveira', 'speeddemon@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
 
-INSERT INTO properties (owner_id, title, description, thumbnail_photo, cover_photo, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
 VALUES (1, 'The Mansion', 'description', 'https://images.pexels.com/photos/1756826/pexels-photo-1756826.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/1756826/pexels-photo-1756826.jpeg', 2000, 10, 5, 5, 'Canada', '834 122 Road', 'West Vancouver', 'BC', 53444, true),
 (2, 'The Cabin', 'description', 'https://images.pexels.com/photos/1756826/pexels-photo-1756826.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/1756826/pexels-photo-1756826.jpeg', 200, 4, 2, 2, 'Canada', '944 455 Road', 'Oliver', 'BC', 58224, true),
 (3, 'The Penthouse', 'description', 'https://images.pexels.com/photos/1756826/pexels-photo-1756826.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/1756826/pexels-photo-1756826.jpeg', 800, 2, 2, 2, 'Canada', '109 654 Road', 'Richmond', 'BC', 69265, true);
@@ -15,7 +15,7 @@ VALUES (1, '2018-09-11', '2018-09-26', 1, 1),
 (3, '2021-10-01', '2021-10-14', 3, 3);
 
 INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
-VALUES (1, 1, 1, 1, 1, 'messages'),
-(2, 2, 2, 2, 2, 'messages'),
-(3, 3, 3, 3, 3, 'messages');
+VALUES (1, 1, 1, 1, 'messages'),
+(2, 2, 2, 2, 'messages'),
+(3, 3, 3, 3, 'messages');
 
